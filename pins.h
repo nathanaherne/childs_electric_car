6e02afa4a8d27653d5dbea5117079c0b23cce8d5
Nathan Aherne <nathan@reddog.com.au>
2018-07-01 16:42:26 +1000

    updates

--- pins.h
+++ pins.h
@@ -2,4 +2,3 @@
 const int MC_forward_pin = PB0; // Forward (analog & digital)
 const int MC_reverseEnable_pin = PC13; // Reverse Enable OR Reverse (analog)
 const int inhibit_pin = PB2; // pin that inhibits drive OR emergency shutdown
-const int NOT_A_PIN = -1;
